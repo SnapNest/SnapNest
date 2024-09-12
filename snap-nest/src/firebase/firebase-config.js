6dc89880397d19afda616951d0863e19d153cd72
Leharov <viktorleharov@gmail.com>
2024-09-12 15:33:18 +0300

    firebase added auth and database

---
+++ snap-nest/src/firebase/firebase-config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLHi6iHVLIwZyrLDu61RrYZQiL5u8UJpk",
+  authDomain: "snapnest-21282.firebaseapp.com",
+  projectId: "snapnest-21282",
+  storageBucket: "snapnest-21282.appspot.com",
+  messagingSenderId: "62928298601",
+  appId: "1:62928298601:web:62fecf2292d5a098edf8f8",
+  measurementId: "G-XZK3BED8TW",
+  databaseURL: "https://snapnest-21282-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const database = getDatabase(app);
