d4efccf997c88c19ddfdc7d17ededd7d3d7384bb
Leharov <viktorleharov@gmail.com>
2024-10-09 10:28:26 +0300

    home page protoype

--- snap-nest/src/components/navbar/Navbar.jsx
+++ snap-nest/src/components/navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 export default function Navbar() {
     return (
         <>
-         <div className="navbar bg-[#283618d8] fixed top-0 left-0 w-full">
-          <div className="logo">
+         <div className="navbar bg-[#283618d8] fixed top-0 left-0 w-full border-b border-[#283618]">
+          <div className="logo text-2xl ml-5">
               SnapNest
           </div>
          </div>
--- snap-nest/src/pages/home/Home.jsx
+++ snap-nest/src/pages/home/Home.jsx
@@ -4,12 +4,18 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-row items-center h-80">
-        <div className="flex-1">
+        <div className="flex-1 text-4xl mr-3">
           SnapNest
         </div>
         <div className="w-0.5 h-full bg-[#283618] mx-4"></div>
-        <div className="flex-1">
-          Get started by signing in or signing up
+        <div>
+          <div className="flex-1 text-4xl ml-3">
+            Get started
+              <div className="flex flex-row mt-5">
+                <button className="bg-[#283618] text-white px-4 py-2 rounded-lg mr-1 text-2xl">Register</button>
+                <button className="bg-[#283618] text-white px-4 py-2 rounded-lg ml-1 text-2xl">Log In</button>
+              </div>
+          </div>
         </div>
       </div>
     </>
