816ad70292e0efaef28a571d85a16a1f55e85b69
Leharov <viktorleharov@gmail.com>
2024-10-16 10:47:22 +0300

    added conditional rendering for sidenav and navbar

--- snap-nest/src/App.jsx
+++ snap-nest/src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/home/Home'
-import Navbar from './components/navbar/Navbar'
-import Main from './pages/main/Main'
+import Home from './pages/home/Home';
+import Navbar from './components/navbar/Navbar';
+import Main from './pages/main/Main';
 import Sidenav from './components/sidenav/Sidenav';
-import { AuthProvider } from './state/authcontext/AuthContext';
+import { AuthProvider, useAuth } from './state/authcontext/AuthContext';
 
-function App() {
+function AppContent() {
+  const { userLoggedIn } = useAuth();
 
   return (
-    <>
-    <AuthProvider>
-      <BrowserRouter>
-        <div className='flex flex-col w-full h-screen'>
-        <Navbar/>
-          <div className="content flex flex-row w-full h-screen">
-            <Sidenav />
-            <div className="flex flex-grow h-full">
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/main' element={<Main />} />
-            </Routes>
-          </div>
-          </div>
-          </div>
-      </BrowserRouter>
-    </AuthProvider>
-    </>
-  )
+    <div className='flex flex-col w-full h-screen'>
+      <Navbar />
+      <div className="content flex flex-row w-full h-screen">
+        {userLoggedIn && <Sidenav />}
+        <div className="flex flex-grow h-full">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/main' element={<Main />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
--- snap-nest/src/components/navbar/Navbar.jsx
+++ snap-nest/src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { auth } from '../../firebase/firebase-config';
 export default function Navbar() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const navigate = useNavigate();
-    const { setCurrentUser } = useAuth();
+    const { setCurrentUser, userLoggedIn } = useAuth();
 
     const toggleDropdown = () => {
         setIsDropdownOpen(!isDropdownOpen);
@@ -30,27 +30,29 @@ export default function Navbar() {
                     SnapNest
                 </div>
                 <div className="flex-grow"></div>
-                <div className="dropdownbtn mr-5">
-                    <div className="dropdown">
-                        <div 
-                            tabIndex={0} 
-                            role="button" 
-                            className="btn m-1 px-6 py-3 text-lg"
-                            onClick={toggleDropdown}
-                        >
-                            User
-                        </div>
-                        {isDropdownOpen && (
-                            <ul 
+                {userLoggedIn && (
+                    <div className="dropdownbtn mr-5">
+                        <div className="dropdown">
+                            <div 
                                 tabIndex={0} 
-                                className="text-lg dropdown-content menu rounded-box border border-[#283618] bg-[#ece6ba] z-[1] w-full p-2 shadow"
+                                role="button" 
+                                className="btn m-1 px-6 py-3 text-lg"
+                                onClick={toggleDropdown}
                             >
-                                <button className="menu-item">Profile</button>
-                                <button onClick={handleLogout} className="menu-item">Logout</button>
-                            </ul>
-                        )}
+                                User
+                            </div>
+                            {isDropdownOpen && (
+                                <ul 
+                                    tabIndex={0} 
+                                    className="text-lg dropdown-content menu rounded-box border border-[#283618] bg-[#ece6ba] z-[1] w-full p-2 shadow"
+                                >
+                                    <button className="menu-item">Profile</button>
+                                    <button onClick={handleLogout} className="menu-item">Logout</button>
+                                </ul>
+                            )}
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
         </>
     );
