762e65624aa88d7651503e98e2db661aa7dd73b4
Leharov <viktorleharov@gmail.com>
2024-10-30 16:57:23 +0200

    edit post impl

--- snap-nest/src/components/post/Post.jsx
+++ snap-nest/src/components/post/Post.jsx
@@ -2,9 +2,10 @@ import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useAuth } from '../../state/authcontext/AuthContext';
 import { ref, get, update } from 'firebase/database';
-import { database } from '../../firebase/firebase-config';
+import { database, storage } from '../../firebase/firebase-config';
 import { useNavigate } from 'react-router-dom';
 import Comment from '../comment/Comment';
+import { getDownloadURL, uploadBytesResumable } from 'firebase/storage';
 import defaultUser from '../../photos/defaultUser.jpg';
 
 const Post = ({ title, name, image, description, postId, userId }) => {
@@ -15,6 +16,10 @@ const Post = ({ title, name, image, description, postId, userId }) => {
     const [comment, setComment] = useState('');
     const [visibleComments, setVisibleComments] = useState(2);
     const [photoURL, setPhotoURL] = useState(defaultUser);
+    const [isEditing, setIsEditing] = useState(false);
+    const [newTitle, setNewTitle] = useState(title);
+    const [newDescription, setNewDescription] = useState(description);
+    const [newImage, setNewImage] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -94,22 +99,124 @@ const Post = ({ title, name, image, description, postId, userId }) => {
         navigate(`/userdetails/${userId}`);
     };
 
+    const handleEditPost = () => {
+        setIsEditing(true);
+    };
+
+    const handleSavePost = async () => {
+        const postRef = ref(database, `posts/${postId}`);
+        let updatedPhotoURL = image;
+
+        if (newImage) {
+            const photoRef = storage.ref(storage, `posts/${userId}/${newImage.name}`);
+            const uploadTask = uploadBytesResumable(photoRef, newImage);
+
+            try {
+                await new Promise((resolve, reject) => {
+                    uploadTask.on(
+                        'state_changed',
+                        null,
+                        (error) => {
+                            console.error('Error uploading file:', error);
+                            reject(error);
+                        },
+                        async () => {
+                            updatedPhotoURL = await getDownloadURL(uploadTask.snapshot.ref);
+                            resolve();
+                        }
+                    );
+                });
+            } catch (error) {
+                console.error('Failed to upload photo:', error);
+                return;
+            }
+        }
+
+        try {
+            await update(postRef, {
+                title: newTitle,
+                content: newDescription,
+                photoURL: updatedPhotoURL
+            });
+            setIsEditing(false);
+        } catch (error) {
+            console.error('Error updating post:', error);
+        }
+    };
+
     return (
         <div className="card w-full h-auto bg-base-100 shadow-xl mb-4">
             <div className="card-body">
-                <div className="flex items-center">
-                    <img src={photoURL} alt="Profile" className="w-8 h-8 rounded-full mr-2" />
-                    <button className="text-blue-500" onClick={handleAuthorClick}>
-                        {name}
-                    </button>
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                        <img src={photoURL} alt="Profile" className="w-8 h-8 rounded-full mr-2" />
+                        <button className="text-blue-500" onClick={handleAuthorClick}>
+                            {name}
+                        </button>
+                    </div>
+                    {currentUser && currentUser.uid === userId && (
+                        <button className="btn btn-sm btn-primary" onClick={handleEditPost}>
+                            Edit Post
+                        </button>
+                    )}
                 </div>
-                <h2 className="text-xl font-bold mt-2">{title}</h2>
-                {image && (
-                    <figure>
-                        <img src={image} alt="Post" className="w-full h-auto" />
-                    </figure>
+                {isEditing ? (
+                    <div className="mt-4">
+                        <div className="form-control">
+                            <label className="label" htmlFor="new-title">
+                                <span className="label-text">Title</span>
+                            </label>
+                            <input
+                                type="text"
+                                id="new-title"
+                                className="input input-bordered"
+                                value={newTitle}
+                                onChange={(e) => setNewTitle(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-control mt-4">
+                            <label className="label" htmlFor="new-description">
+                                <span className="label-text">Description</span>
+                            </label>
+                            <textarea
+                                id="new-description"
+                                className="textarea textarea-bordered h-24"
+                                value={newDescription}
+                                onChange={(e) => setNewDescription(e.target.value)}
+                            ></textarea>
+                        </div>
+                        <div className="form-control mt-4">
+                            <label className="label" htmlFor="new-image">
+                                <span className="label-text">Attach Photo</span>
+                            </label>
+                            <input
+                                type="file"
+                                id="new-image"
+                                className="input input-bordered"
+                                accept="image/*"
+                                onChange={(e) => setNewImage(e.target.files[0])}
+                            />
+                        </div>
+                        <div className="flex justify-end mt-4">
+                            <button className="btn btn-secondary mr-2" onClick={handleSavePost}>
+                                Save
+                            </button>
+                            <button className="btn btn-ghost" onClick={() => setIsEditing(false)}>
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                ) : (
+                    <>
+                        <h2 className="text-xl font-bold mt-2">{title}</h2>
+                        {image && (
+                            <figure>
+                                <img src={image} alt="Post" className="w-full h-auto" />
+                            </figure>
+                        )}
+                        <p>{description}</p>
+                    </>
                 )}
-                <p>{description}</p>
                 <div className="flex items-center mt-4">
                     <button className={`btn ${likedByUser ? 'btn-secondary' : 'btn-primary'} mr-4`} onClick={handleLike}>
                         {likedByUser ? 'Unlike' : 'Like'} ({likes})
